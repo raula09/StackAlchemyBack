8fbcba48bcde24c6c1283ca664f23d5ad9104dc2
Nika Egiazarovi <151972349+NikaEgiazarovi@users.noreply.github.com>
2025-03-03 20:00:09 +0400

    this is a model for database

--- StackAlchemy-Back/Models/Question.cs
+++ StackAlchemy-Back/Models/Question.cs
@@ -6,33 +6,14 @@ namespace StackAlchemy_Back.Models
 {
     public class Question
     {
-        [Key]
         public int Id { get; set; }
-
-        [Column(TypeName = "nvarchar(200)")]
-        public string Title { get; set; }
-
-        [Column(TypeName = "nvarchar(max)")]
-        public string Description { get; set; }
-
-        [Column(TypeName = "nvarchar(max)")]
-        public string Code { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public int Views { get; set; }
-        public int Score { get; set; }
-        public bool IsClosed { get; set; }
-
-        [ForeignKey("User")]
-        public int UserId { get; set; }
+        public int User_Id { get; set; }
         public User User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? Code { get; set; }
+        public int Scores { get;set; }
 
-        public ICollection<Answer> Answers { get; set; }
-        public ICollection<Comment> Comments { get; set; }
-        public ICollection<Tag> Tags { get; set; }
-        public ICollection<Vote> Votes { get; set; }
+
     }
-
-    //test comment ss
 }
