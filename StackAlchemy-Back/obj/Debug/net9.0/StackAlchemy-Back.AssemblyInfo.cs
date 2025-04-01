c8a219763310e4bba0649ac61eca7334d110d434
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-04-01 10:22:51 +0400

    idk

--- StackAlchemy-Back/obj/Debug/net9.0/StackAlchemy-Back.AssemblyInfo.cs
+++ StackAlchemy-Back/obj/Debug/net9.0/StackAlchemy-Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StackAlchemy-Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0857c488ca83bd6e3fa424013fb82e9ec646c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168107ac082b10498d403e13ee058095df83dcbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StackAlchemy-Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StackAlchemy-Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
